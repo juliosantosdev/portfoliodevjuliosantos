be46a545f86d0730044896850cadb9fb152ffdc8
juliosantosdev <145381879+juliosantosdev@users.noreply.github.com>
2023-11-02 00:20:23 -0300

    commit2

---
+++ script.js
@@ -0,0 +1,11 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "Images/menu.png";
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "Images/close-menu.png";
+    }
+    
+}
