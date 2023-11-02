44e648f1560962d9a5330e43000d0d4e3b1c6e1f
juliosantosdev <145381879+juliosantosdev@users.noreply.github.com>
2023-11-02 14:51:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ function menuShow() {
         menuMobile.classList.add('open')
         document.querySelector('.icon').src = "Images/close-menu.png";
     }
-    
+
 }
+
+window.addEventListener('load', function() {
+    var source = document.createElement('source');
+    source.src = 'video/1menor.mp4';
+    source.type = 'video/mp4';
+
+    var video = document.createElement('video');
+    video.appendChild(source);
+    video.autoplay = true;
+    video.loop = true;
+
+    var videoContainer = document.querySelector('.video-sobre');
+    videoContainer.appendChild(video);
+
+    video.load();
+});
