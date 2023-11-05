49dd309712c73ffa59957b5214dd317e71b4964c
juliosantosdev <145381879+juliosantosdev@users.noreply.github.com>
2023-11-05 18:41:01 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,138 @@
+window.revelar = ScrollReveal ({reset:false})
+
+// TOPO DO SITE //
+
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 700
+})
+
+// Efeito nos Titulos//
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top'
+})
+
+
+
+// Efeito Especialidades//
+revelar.reveal('.efeito-especialidades1',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1500
+})
+
+// Efeito Sobre //
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-video-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+// Efeito Portfolio//
+
+revelar.reveal('.efeito-img-port1',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 500
+})
+
+revelar.reveal('.efeito-img-port2',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-img-port3',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 1500
+    
+})
+
+// Efeito Formulario//
+
+revelar.reveal('.efeito-nome',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 500
+})
+
+revelar.reveal('.efeito-email',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-celular',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-mensagem',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 2000
+})
+
+revelar.reveal('.efeito-btn-enviar',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'top',
+    delay: 2500
+})
+
+
+
--- script.js
+++ script.js
@@ -10,21 +10,3 @@ function menuShow() {
 
 }
 
-// Obtém o botão
-var mybutton = document.getElementById("myBtn");
-
-// Quando o usuário rola para baixo 20px da parte superior do documento, mostra o botão
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// Quando o usuário clica no botão, rola para o topo do documento
-function topFunction() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-}
